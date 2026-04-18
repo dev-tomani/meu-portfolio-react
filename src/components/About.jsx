bad25fc0cb600c4c768b647b17a898c1c46483a1
dev-tomani <dev.andersonarodriges2@gmail.com>
2026-04-17 23:50:18 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,9 +13,8 @@ const About = () => {
         </div>
         <div className="about-text">
           <h3 style={{ textAlign: 'center' }}>
-            Comecei minha vida, fiz um curso básico de informática, mas foi na musica que eu me achei, hoje sou músico e produtor musical.<br /> 
-            Mas sei que devemos evoluir e aceitei o desafio!<br /> 
-            Estou tentando o meu primeiro curso superior na area da Tecnologia, pois a mesma move o mundo!
+            Sou um profissional de 29 anos, estudante de Sistemas para Internet, iniciei esta nova fase da vida, buscando  uma fonte de renda remota ou híbrida que me possa dar estabilidade, crescimento a longo prazo e qualidade de vida. <br/>
+            Atualmente, tento conciliar o trabalho, com os estudos, focando os meus conhecimentos na transição de carreira para a área de Tecnologia.
           </h3>
         </div>
       </div>
